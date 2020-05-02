0a3879a6887b780cfd1409cae11ccaf90911f19b
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-02 21:02:16 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,27 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
+  attr_accessor :name, :location, :profile_url
+
   def self.scrape_index_page(index_url)
-    
+    students = Nokogiri::HTML(open(index_url))
+    students.css("div.roster-cards-container").flat_map do |student|
+      student.css("div.student-card").collect do |person|
+        @name = person.css("h4.student-name").text
+        @location = person.css("p.student-location").text
+        @profile_url = person.css("div.a href").text
+        {:name => @name, :location => @location, :profile_url => @profile_url}
+
+  end
+  end
+binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
