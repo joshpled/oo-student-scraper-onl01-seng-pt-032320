a3d2d65ca0639959b24c9502e6ffe86341835ef2
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-05 00:58:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 class Scraper
 
-  attr_accessor :name, :location, :profile_url
+  attr_accessor :name, :location, :profile_url, :twitter, :linkedin, :github, :blog, :profile_quote, :bio
 
   def self.scrape_index_page(index_url)
     students = Nokogiri::HTML(open(index_url))
@@ -12,16 +12,35 @@ class Scraper
       student.css("div.student-card").collect do |person|
         @name = person.css("h4.student-name").text
         @location = person.css("p.student-location").text
-        @profile_url = person.css("div.a href").text
+        @profile_url = person.css("a").attribute("href").text
         {:name => @name, :location => @location, :profile_url => @profile_url}
-
   end
   end
-binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-
+    student = Nokogiri::HTML(open(profile_url))
+    
+    links = student.css("div.social-icon-container").css("a")
+    socials = [].uniq
+    links.flat_map do |i|
+      socials << i.get_attribute("href")
+    end
+    socials.each do |value|
+      case value
+      when /twitter/
+        @twitter = value
+      when /linkedin/
+        @linkedin = value
+      when /github/
+        @github = value
+      else
+        @blog = value
+      end
+    end
+    @profile_quote = student.css("div.profile-quote").text
+    @bio = student.css("div.description-holder").css("p").text
+    {:twitter => @twitter, :linkedin => @linkedin, :github => @github, :blog => @blog, :profile_quote => @profile_quote, :bio => @bio}
   end
 
 end
