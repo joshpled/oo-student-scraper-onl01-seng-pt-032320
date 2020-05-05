2632453beea97d44a122d1ade63cc0776a7489d2
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-05 03:21:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,12 +30,10 @@ class Scraper
         @linkedin = value
       elsif value.match?(/github/)
         @github = value
-      else
-        @blog = value
       end
-    # binding.pry
+    
     end
-
+    binding.pry 
     @profile_quote = student.css("div.profile-quote").text
     @bio = student.css("div.description-holder").css("p").text
     { :twitter => @twitter, :linkedin => @linkedin, :github => @github, :blog => @blog, :profile_quote => @profile_quote, :bio => @bio }
