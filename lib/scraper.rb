389f9e5ad211dab03c70c3cb745036029e91a695
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-05 03:09:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -38,7 +38,7 @@ class Scraper
 
     @profile_quote = student.css("div.profile-quote").text
     @bio = student.css("div.description-holder").css("p").text
-    {:twitter => @twitter, :linkedin => @linkedin, :github => @github, :blog => @blog, :profile_quote => @profile_quote, :bio => @bio}
+    { :twitter => @twitter, :linkedin => @linkedin, :github => @github, :blog => @blog, :profile_quote => @profile_quote, :bio => @bio }
 
   end
 
